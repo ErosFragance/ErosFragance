eedbced5bc4b2954104c7cd53deb24493296d260
ErosFragance <121646238+ErosFragance@users.noreply.github.com>
2023-01-04 22:14:28 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".fragancias")
+const menuHambur = document.querySelector(".menu")
+
+
+menuHambur.addEventListener("click",()=>{
+    menu.classList.toggle("desplazado")
+})
